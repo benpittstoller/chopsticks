b11b04fe2c7763990e53da91a845fd1a719a782a
Ben Pitt Stoller <benpittstoller@gmail.com>
2024-04-11 15:20:26 -0400

    glitch test

--- server/index.js
+++ server/index.js
@@ -20,6 +20,9 @@ const io = new Server(server, {
 io.on("connection", socket => {
 	console.log(`User connected: ${socket.id}`);
 
+	socket.on("disconnect", () => {
+		console.log(`User disconnected: ${socket.id}`);
+	});
 	socket.on("sendMessage", data => {
 		socket.broadcast.emit("receiveMessage", data);
 	});
